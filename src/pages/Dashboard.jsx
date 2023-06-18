5c815cadf4ce3d60da527c5d8c0f05733bcf580b
Pradeep kumar <117191675+Techi-pradeep@users.noreply.github.com>
2023-06-18 19:49:53 +0530

    Grid vs SimpleGrid

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -21,3 +21,10 @@ export default function Dashboard() {
     </SimpleGrid>
   )
 }
+
+/*Grid vs SimpleGrid
+A Grid component allows you to create a complex grid layout with multiple rows and columns. You can customize the sizing of each grid item and adjust the gap between them. This component does not have a default responsive behavior.
+
+On the other hand, a SimpleGrid is a simpler version of the Grid and provides a default responsive behavior. It automatically adjusts the number of columns based on the screen size, making it easier to create responsive grid layouts. Additionally, the SimpleGrid component provides some basic styling out of the box, such as padding and spacing between grid items.
+
+*/
