fed2c60c09e13b2da54dc8e314b1dcc2c06a78c9
Pradeep kumar <117191675+Techi-pradeep@users.noreply.github.com>
2023-06-18 19:51:21 +0530

    Grid + Grid items  not auto responsive

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,6 +5,8 @@ import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
 
 export default function RootLayout() {
+/* Grid more complex but not auto responsive*/
+  
   return (
     <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
       {/* sidebar */}
