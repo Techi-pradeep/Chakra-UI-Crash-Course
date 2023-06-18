b25cdcf90d653b89c5a9be16cd81b4554775510a
Pradeep kumar <117191675+Techi-pradeep@users.noreply.github.com>
2023-06-18 18:15:27 +0530

    as , HStack with spacing, bg="color.range", gap , colorSheme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,3 +21,13 @@ export default function Navbar() {
     // </Flex>
   )
 }
+
+/*In the provided context, `HStack` is a Chakra UI component used to horizontally stack its child elements, with a specified spacing between the elements.
+
+`as` is a prop that can be passed to the `Flex` component to indicate a different HTML element to render instead. In this case, the `as` prop is set to "nav" to indicate that the `Flex` element should be rendered as a `<nav>` element.
+
+`ColorScheme` is a prop that can be passed to the `Button` component. It is used to set the background color and text color of the button based on a pre-defined color scheme provided by Chakra UI, such as "purple" in this case. 
+spacing is a prop that is used with the HStack component to define the space between its child elements horizontally.
+
+gap is a prop that is used with the Flex component to define the space between its child elements without growing the elements themselves. It is used to create gaps between items in a grid-like layout, where each item has a fixed width and height.
+*/
